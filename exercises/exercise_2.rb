be615e4ae1c3530150b1527394fd0195f910ebb8
Jeffrey <jeffrey-lee@live.ca>
2022-09-21 15:47:49 +0000

    exercise 2 compelted

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -13,11 +13,3 @@ puts "----------"
 @store1.name = "Seaside mall"
 
 @store1.save
-#puts @store1
-
-
-# 1. Load the first store (with `id = 1`) from the database and
-#  assign it to an instance variable `@store1`.
-# 2. Load the second store from the database and assign it to `@store2`.
-# 3. Update the first store (`@store1`) instance in the database. 
-# (Change its name or something.)
